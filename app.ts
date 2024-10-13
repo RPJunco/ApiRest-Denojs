410ede063dd71bad4f442711d20d20c57defab05
RPJunco <rodrigojunco023@gmail.com>
2024-10-13 13:22:28 -0300

    Se creo una interfaz 'Perro', se creo un array que crea instancias de esa interfaz y se crearon funciones para manejar el metodo get

--- app.ts
+++ app.ts
@@ -3,15 +3,53 @@ import {Application, Router} from 'https://deno.land/x/oak/mod.ts'
 const app = new Application();
 const router = new Router();
 
+
+interface Perro {
+    nombre: string
+    edad: number
+
+}
+let perros : Array<Perro> = [
+    {nombre: "Boby", edad: 1},
+    {nombre: "Rafa", edad: 4},
+    {nombre: "Rex", edad: 3},
+    {nombre: "Mara", edad: 1}
+];
+
+export const getPerros = ({response}: {response: any}) =>{
+    response.status = 200
+    response.body = perros
+    return
+}
+
+export const getPerro = ({
+    params,
+    response
+}: {
+    params:{
+        nombre: string
+    },
+    response:any
+}) => {
+    const perro = perros.filter((perro) => perro.nombre === params.nombre)
+    if (perro.length){
+        response.status = 200
+        response.body = perro[0]
+        return
+    }
+    response.status = 400
+    response.body = { msj: 'Perro no encontrado' }
+}
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-// router
-//     .get('/perros', getPerros)
-//     .get('/perros/:nombre', getPerros)
-//     .post('/perros', addPerro)
-//     .put('/perros/:nombre', updatePerro)
-//     .delete('/perros/:nombre', removePerro);
+router
+    .get('/perros', getPerros)
+    .get('/perros/:nombre', getPerro)
+    // .post('/perros', addPerro)
+    // .put('/perros/:nombre', updatePerro)
+    // .delete('/perros/:nombre', removePerro);
 
 const env = Deno.env.toObject();
 const PORT = env.PORT || 4000
